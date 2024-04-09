3070f54187786fb37abc582fe86a731a45785860
Eujeen Han <eujeenhan@gmail.com>
2024-04-09 10:47:23 +0900

    Add HTTP request function and update Storyboard

--- LittleQuery/LittleQuery/Controllers/GoogleScholarTableViewController.swift
+++ LittleQuery/LittleQuery/Controllers/GoogleScholarTableViewController.swift
@@ -8,6 +8,37 @@
 import UIKit
 
 class GoogleScholarTableViewController: UITableViewController {
+    let apiKey = "3e0d360330ffda3410e04ebabbdcae1068124e880e5fcbb3b2c75ba385d4e0e7"
+    var results: [Article]?
+    
+    func buildRequest(query: String, page: Int, size: Int) -> URLRequest {
+        let endpoint = "https://serpapi.com/search?engine=google_scholar&q=\(query)&hl=en&start=\(page)&api_key=\(apiKey)&num=\(size)"
+        
+        guard let endpoint = endpoint.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: endpoint)
+        else { fatalError() }
+        
+        return URLRequest(url: url)
+    }
+    
+    func createTask(query: String?, page: Int, size: Int) {
+        guard let query else { return }
+        let request = buildRequest(query: query, page: page, size: size)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error { print(error.localizedDescription); return }
+            
+            guard let data else { return }
+            
+            let root = try? JSONDecoder().decode(Root.self, from: data)
+            self.results = root?.results
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }.resume()
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +48,7 @@ class GoogleScholarTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        createTask(query: "biology", page: 0, size: 5)
     }
 
     // MARK: - Table view data source
@@ -28,7 +60,7 @@ class GoogleScholarTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return results?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -36,7 +68,11 @@ class GoogleScholarTableViewController: UITableViewController {
 
         // Configure the cell...
         let titleLabel = cell.viewWithTag(1) as? UILabel
-        titleLabel?.text = "A new biology for a new century"
+        titleLabel?.text = results?[indexPath.row].title
+        let publicationLabel = cell.viewWithTag(2) as? UILabel
+        publicationLabel?.text = results?[indexPath.row].publication["summary"]
+        let snippetLabel = cell.viewWithTag(3) as? UILabel
+        snippetLabel?.text = results?[indexPath.row].snippet
 
         return cell
     }
@@ -52,6 +88,7 @@ class GoogleScholarTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         if segue.identifier == "articleSegue" {
+            let controller = segue.destination as? GSDetailViewController
         }
     }
 
